ad15f471d677350a288c5749bc28dfaaf0022f87
ktennis <kelseycorfield@gmail.com>
2024-04-08 18:44:48 -0600

    Order Confrimation page

--- Legos/Controllers/HomeController.cs
+++ Legos/Controllers/HomeController.cs
@@ -40,6 +40,11 @@ namespace Legos.Controllers
             return View();
         }
 
+        public IActionResult OrderConfirmation()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
