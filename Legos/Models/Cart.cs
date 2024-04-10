87107cf3c1ccd89c27d2e8a896a6d9a5b4d74705
ktennis <kelseycorfield@gmail.com>
2024-04-10 12:22:43 -0600

    cart

---
+++ Legos/Models/Cart.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Evaluation;
+
+namespace Legos.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+        public void AddItem(Product p, int quantity)
+        {
+            CartLine? Line = Lines
+                .Where(x => x.Product.ProductId == p.ProductId)
+                .FirstOrDefault();
+            if (Line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = p,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                Line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLive(Product p) => Lines.RemoveAll(x => x.Product.ProductId == p.ProductId);
+
+        public void Clear() => Lines.Clear();
+        public Decimal CalculateTotal() => (decimal)Lines.Sum(x => x.Product.Price * x.Quantity);
+        public class CartLine
+        {
+            public int CartLineId { get; set; }
+            public Product Product { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
--- Legos/Views/Home/Cart.cshtml.cs
+++ Legos/Views/Home/Cart.cshtml.cs
@@ -1,7 +1,31 @@
+using Legos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Legos.Views.Home
 {
+    public class CartModel : PageModel
+    {
+        private ILegosRepository _repo;
+
+        public CartModel(ILegosRepository temp)
+        {
+            _repo = temp;
+        }
+        public Cart? Cart { get; set; }
+
+        public void OnGet()
+        {
+        }
+        public void OnPost(int productId) 
+        { 
+            Product p = _repo.Products
+                .FirstOrDefault(x => x.ProductId == productId);
+            
+            Cart = new Cart();
+
+            Cart.AddItem(p, 1);
+        }
+    }
 
 }
