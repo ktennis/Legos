fc08c4f5ed4a0546e82cbbbd56ffce443c01840d
ktennis <kelseycorfield@gmail.com>
2024-04-11 13:47:36 -0600

    delete button!

---
+++ Legos/Models/SessionCart.cs
@@ -0,0 +1,44 @@
+﻿using Legos.Infastructure;
+using Legos.Models;
+using System.Text.Json.Serialization;
+
+namespace Legos.Models
+{
+    public class SessionCart : Cart
+    {
+        public static Cart GetCart (IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()
+                .HttpContext?.Session;
+
+            SessionCart cart = session?.GetJson<SessionCart>("Cart") ?? 
+                new SessionCart();
+            
+            cart.Session = session;
+            
+            return cart;
+        }
+
+        [JsonIgnore]
+        public ISession? Session { get; set; }
+
+        public override void AddItem(Product p, int quantity)
+        {
+            base.AddItem(p, quantity);
+            Session?.SetJson("Cart", this);
+        }
+
+        public override void RemoveLine(Product p)
+        {
+            base.RemoveLine(p);
+            Session?.SetJson("Cart", this);
+        }
+        public override void Clear()
+        {
+            base.Clear();
+            Session?.Remove("Cart");
+        }
+    }
+}
+
+
