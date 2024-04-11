8b4ba5a3d267f5c4fe3edcfc0f442864df4cf5a6
Zachhansen4455 <120439561+Zachhansen4455@users.noreply.github.com>
2024-04-11 11:57:55 -0600

    Product recommendations

---
+++ Legos/Models/ViewModels/ProductDetailsViewModel.cs
@@ -0,0 +1,11 @@
+namespace Legos.Models.ViewModels;
+
+public class ProductDetailsViewModel
+{
+    public Product Product { get; set; }
+    public Product RecommendedProduct1 { get; set; }
+    public Product RecommendedProduct2 { get; set; }
+    public Product RecommendedProduct3 { get; set; }
+    public Product RecommendedProduct4 { get; set; }
+    public Product RecommendedProduct5 { get; set; }
+}
